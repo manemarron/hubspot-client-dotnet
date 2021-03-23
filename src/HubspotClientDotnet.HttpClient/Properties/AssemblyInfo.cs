896dfffbc8cf6cdcf5bba25c5069bb5c5bbf584f
Jose Manuel Marron <jmmarron@gbm.com.mx>
2021-03-23 10:51:08 -0600

    Add configuration to sign assemblies (key remains private)

--- src/HubspotClientDotnet.HttpClient/Properties/AssemblyInfo.cs
+++ src/HubspotClientDotnet.HttpClient/Properties/AssemblyInfo.cs
@@ -6,4 +6,4 @@
 //-----------------------------------------------------------------------
 using System.Runtime.CompilerServices;
 
-[assembly:InternalsVisibleTo("HubspotClientDotnet.HttpClient.UnitTests")]
+[assembly: InternalsVisibleTo("HubspotClientDotnet.HttpClient.UnitTests,PublicKey=0024000004800000940000000602000000240000525341310004000001000100ebe5126e74ad7df940d074d8b6f71e3399501636fab2d3ec7398c91ac850c11baa80a349ebae15aeb4717e06680609f31a9474c768590d9e889e512b27c4c6be3aabcf29388fe80536881bd58545634677a6d5173242b6144e49a76387dd201926b940a5c66694297b96f8dc4d6ae31f796dcac6b7b5599a31538ee455b2c08a")]
